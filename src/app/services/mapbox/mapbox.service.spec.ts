dfa4ccc40e8e39aad0cdc124181c809ffe81c04d
mohammed_dev <mohammed_dev@yahoo.com>
2022-11-09 14:44:36 -0600

    feat(5): clean up code and fix testing files

--- src/app/services/mapbox/mapbox.service.spec.ts
+++ src/app/services/mapbox/mapbox.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { MapboxService } from './mapbox.service';
 
@@ -6,7 +7,9 @@ describe('MapboxService', () => {
   let service: MapboxService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(MapboxService);
   });
 
